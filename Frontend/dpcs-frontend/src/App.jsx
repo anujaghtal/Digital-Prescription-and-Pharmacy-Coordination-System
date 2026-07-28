16384e4109f84a4e336d602500be132708c0bd13
Anuja <anujaghtal@gmail.com>
2026-07-29 02:03:08 +0530

    Implement protected routes for authenticate users

--- Frontend/dpcs-frontend/src/App.jsx
+++ Frontend/dpcs-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 import AdminDashboard from "./pages/admin/Dashboard";
 import DoctorDashboard from "./pages/doctor/Dashboard";
 import PatientDashboard from "./pages/patient/Dashboard";
@@ -16,15 +17,43 @@ function App() {
 
                 <Route path="/" element={<Login />} />
 
-                <Route path="/admin" element={<AdminDashboard />} />
+                <Route
+                  path="/admin"
+                  element={
+                      <ProtectedRoute>
+                          <AdminDashboard />
+                      </ProtectedRoute>
+                  }
+              />
 
-                <Route path="/doctor" element={<DoctorDashboard />} />
+              <Route
+                  path="/doctor"
+                  element={
+                      <ProtectedRoute>
+                          <DoctorDashboard />
+                      </ProtectedRoute>
+                  }
+              />
 
-                <Route path="/patient" element={<PatientDashboard />} />
+              <Route
+                  path="/patient"
+                  element={
+                      <ProtectedRoute>
+                          <PatientDashboard />
+                      </ProtectedRoute>
+                  }
+              />
 
+              <Route
+                  path="/pharmacist"
+                  element={
+                      <ProtectedRoute>
+                          <PharmacistDashboard />
+                      </ProtectedRoute>
+                  }
+              />
                 <Route path="/register" element={<Register />} />
-                <Route path="/pharmacist" element={<PharmacistDashboard />} />
-
+      
             </Routes>
 
         </BrowserRouter>
