dff43ad634301909bffbafd5f8258f8929b513d6
Anuja <anujaghtal@gmail.com>
2026-07-29 00:12:58 +0530

    Implement login page

---
+++ Frontend/dpcs-frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import api from "../../api/api";
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+
+    const [password, setPassword] = useState("");
+
+    const login = async (e) => {
+
+        e.preventDefault();
+
+        try {
+
+            const response = await api.post("/auth/login", {
+
+                email,
+                password
+
+            });
+
+            console.log(response.data);
+
+        } catch (error) {
+
+            alert("Invalid Credentials");
+
+        }
+
+    };
+
+    return (
+
+        <div className="container mt-5">
+
+            <div className="row justify-content-center">
+
+                <div className="col-md-4">
+
+                    <div className="card p-4">
+
+                        <h3 className="text-center mb-3">
+
+                            DPCS Login
+
+                        </h3>
+
+                        <form onSubmit={login}>
+
+                            <input
+                                className="form-control mb-3"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e)=>setEmail(e.target.value)}
+                            />
+
+                            <input
+                                type="password"
+                                className="form-control mb-3"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                            />
+
+                            <button
+                                className="btn btn-primary w-100">
+
+                                Login
+
+                            </button>
+
+                        </form>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default Login;
